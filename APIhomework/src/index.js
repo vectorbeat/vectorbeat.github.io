89bd183537ea6b618d9d863e5a1afa1d0b8fbf8b
ajbdev <andybaird@gmail.com>
2021-03-19 15:55:14 -0600

    Update index.js
    The main takeaway from my changes is to show how redundant the `showTemperatureByCity` function is.  You're basically wrangling / formatting the Axios response object into the correct DOM elements twice when you only need to do it once. In my example, you can see how the showTemperature function gets reused by both API calls. 
    By having only one function that actually updates the HTML DOM elements you'll reduce the chance that you have a bug. For example, if you decided to rename the class element for the country or city div containers, you'd need to remember to update it in only one spot instead of two.

--- APIhomework/src/index.js
+++ APIhomework/src/index.js
@@ -3,19 +3,23 @@ function getCurrentPosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-  axios.get(url).then(showTemperature);
+  axios.get(url).then(function(response) {
+    showTemperature(response.data.main.temp, response.data.name, response.data.sys.country);
+  });
 }
-
-function showTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let currentTemp = document.querySelector(".currently-temp");
-  currentTemp.innerHTML = `${temperature}°F`;
-  let currentCity = document.querySelector(".currently-city");
-  let currentCityName = response.data.name;
-  currentCity.innerHTML = `${currentCityName}`;
-  let country = document.querySelector(".currently-country");
-  let currentCountryName = response.data.sys.country;
-  country.innerHTML = `${currentCountryName}`;
+                      
+function showTemperature(temp, country, city = null) {
+  const temperature = Math.round(temp);
+  const city = city.trim();
+  const country = country.trim();
+    
+  const temperatureEl = document.querySelect('.currently-temp');
+  const currentCityEl = document.querySelector('.currently-city');
+  const currentCountryEl = document.querySelector('.currently-country');
+    
+  temperatureEl.innerHTML = `${temperature}°F`;
+  currentCityEl.innerHTML = city;
+  currentCountryEl.innerHTML = country;  
 }
 
 function callNavigator(event) {
@@ -29,16 +33,9 @@ function beginSearch(event) {
   city.innerHTML = `${searchInput.value.trim().toUpperCase()}`;
   let apiK = "fb8b95424c106907f53c4fc0092c4971";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=imperial&appid=${apiK}`;
-  axios.get(apiUrl).then(showTemperatureByCity);
-}
-
-function showTemperatureByCity(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let currentTemp = document.querySelector(".currently-temp");
-  currentTemp.innerHTML = `${temperature}°F`;
-  let country = document.querySelector(".currently-country");
-  let countryName = response.data.sys.country;
-  country.innerHTML = `${countryName}`;
+  axios.get(apiUrl).then(function(response) {
+    showTemperature(response.data.main.temp, response.data.name, response.data.sys.country);
+  });
 }
 
 let useCurrent = document.querySelector("#use-current");
